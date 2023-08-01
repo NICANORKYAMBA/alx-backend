d0d247d03220833c4ace5959458c1b0407a0a3cc
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-01 11:32:39 +0300

    0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue July  01 11:30:00 2023
+
+@Author: Nicanor Kyamba
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Main page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
